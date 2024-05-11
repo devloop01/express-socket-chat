495bc72bc057b4d359151ada0a24bff34df99a30
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-11 15:36:10 +0530

    no socket

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+
+// const app = express();
+
+// app.use(express.static("public"));
+
+// const httpServer = createServer(app);
+// const io = new Server(httpServer);
+
+// io.on("connection", (socket) => {
+//   const id = socket.id;
+
+//   console.log("user connected", id);
+
+//   socket.on("message", (message, callback) => {
+//     const data = { id, message };
+
+//     console.log("message", data);
+
+//     callback(data);
+//     socket.broadcast.emit("message", data);
+//   });
+// });
+
+// httpServer.listen(3000, "0.0.0.0", undefined, () => {
+//   console.log("Server listening on port 3000");
+// });
 
 const app = express();
 
+const PORT = 3000;
+
 app.use(express.static("public"));
 
-const httpServer = createServer(app);
-const io = new Server(httpServer);
-
-io.on("connection", (socket) => {
-  const id = socket.id;
-
-  console.log("user connected", id);
-
-  socket.on("message", (message, callback) => {
-    const data = { id, message };
-
-    console.log("message", data);
-
-    callback(data);
-    socket.broadcast.emit("message", data);
-  });
-});
-
-httpServer.listen(3000, "0.0.0.0", undefined, () => {
-  console.log("Server listening on port 3000");
-});
+app.listen(PORT, "0.0.0.0", () => console.log(`Sever is running port ${PORT} ...`));
