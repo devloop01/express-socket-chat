dd7f8d85272e7a9cf54e4a65083ac424104f5fc5
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-11 15:10:53 +0530

    ✨

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  const id = socket.id;
+
+  console.log("user connected", id);
+
+  socket.on("message", (message, callback) => {
+    const data = { id, message };
+
+    console.log("message", data);
+
+    callback(data);
+    socket.broadcast.emit("message", data);
+  });
+});
+
+httpServer.listen(3000, "0.0.0.0", undefined, () => {
+  console.log("Server listening on port 3000");
+});
