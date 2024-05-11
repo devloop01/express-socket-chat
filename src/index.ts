dc0b56cd829dd4475567529d4ed9ce59bbc69a95
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-11 15:50:49 +0530

    hello

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { resolve } from "path";
 // import { createServer } from "http";
 // import { Server } from "socket.io";
 
-const app = express();
+// const app = express();
 
-app.use(express.static(resolve(__dirname, "../public")));
+// app.use(express.static("public"));
 
 // const httpServer = createServer(app);
 // const io = new Server(httpServer);
@@ -25,6 +24,16 @@ app.use(express.static(resolve(__dirname, "../public")));
 //   });
 // });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server listening on port 3000");
+// httpServer.listen(3000, "0.0.0.0", undefined, () => {
+//   console.log("Server listening on port 3000");
+// });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello there! Api is working");
 });
+
+app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
