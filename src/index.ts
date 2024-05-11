1c9b3f0b9e3d8ee82686539c1498c4eb11ab6b9a
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-11 15:46:19 +0530

    reolve public

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,30 @@
 import express from "express";
-// import { createServer } from "http";
-// import { Server } from "socket.io";
-
-// const app = express();
-
-// app.use(express.static("public"));
-
-// const httpServer = createServer(app);
-// const io = new Server(httpServer);
-
-// io.on("connection", (socket) => {
-//   const id = socket.id;
-
-//   console.log("user connected", id);
-
-//   socket.on("message", (message, callback) => {
-//     const data = { id, message };
-
-//     console.log("message", data);
-
-//     callback(data);
-//     socket.broadcast.emit("message", data);
-//   });
-// });
-
-// httpServer.listen(3000, "0.0.0.0", undefined, () => {
-//   console.log("Server listening on port 3000");
-// });
+import { resolve } from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static(resolve(__dirname, "../public")));
 
-app.get("/", (req, res) => {
-  res.send("Hello there! Api is working");
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  const id = socket.id;
+
+  console.log("user connected", id);
+
+  socket.on("message", (message, callback) => {
+    const data = { id, message };
+
+    console.log("message", data);
+
+    callback(data);
+    socket.broadcast.emit("message", data);
+  });
 });
 
-app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
+httpServer.listen(3000, "0.0.0.0", undefined, () => {
+  console.log("Server listening on port 3000");
+});
