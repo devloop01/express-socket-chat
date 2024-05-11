3bc18b5d9d667a71aec2cc434c876be31f231362
Sikriti Dakua <38714423+devloop01@users.noreply.github.com>
2024-05-11 15:49:21 +0530

    f

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
 import express from "express";
 import { resolve } from "path";
-import { createServer } from "http";
-import { Server } from "socket.io";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.static(resolve(__dirname, "../public")));
 
-const httpServer = createServer(app);
-const io = new Server(httpServer);
+// const httpServer = createServer(app);
+// const io = new Server(httpServer);
 
-io.on("connection", (socket) => {
-  const id = socket.id;
+// io.on("connection", (socket) => {
+//   const id = socket.id;
 
-  console.log("user connected", id);
+//   console.log("user connected", id);
 
-  socket.on("message", (message, callback) => {
-    const data = { id, message };
+//   socket.on("message", (message, callback) => {
+//     const data = { id, message };
 
-    console.log("message", data);
+//     console.log("message", data);
 
-    callback(data);
-    socket.broadcast.emit("message", data);
-  });
-});
+//     callback(data);
+//     socket.broadcast.emit("message", data);
+//   });
+// });
 
-httpServer.listen(3000, "0.0.0.0", undefined, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server listening on port 3000");
 });
